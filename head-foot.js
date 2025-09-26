bd5ff61ae76e87c93e0b34c9b99978cf427c5628
MadMilliner <jasonapeterson@gmail.com>
2025-09-26 14:21:46 -0700

    Small tweaks, underline current page in navbar, signup on each page

--- head-foot.js
+++ head-foot.js
@@ -1,5 +1,5 @@
 const headerContent = () => `
-            <div><a href="index.html"><img src="img/AllSaintsLogo_Pride_KO_Horizontal-01.webp" alt="Home - All Saints Church LA Logo" id="headerlogo"></a></div>
+            <a href="index.html" id="headerLogoA"><img src="img/AllSaintsLogo_Pride_KO_Horizontal-01.webp" alt="Home - All Saints Church LA Logo" id="headerlogo"></a>
             <div id="navbar">
                 <a href="about.html">About</a>
                 <a href="webelieve.html">We Believe</a>
@@ -9,12 +9,31 @@ const headerContent = () => `
                 <a href="jobs.html">Jobs</a>
             </div>
         `
-
+const signupContent = () => `
+                <div class="signup">
+                <p>Sign up with your email address to receive our monthly newsletter.</p>
+                <p><input type="email" autocomplete="email" placeholder="E-mail address">
+                <button type="submit">Sign-Up</button></p>
+                </div>
+        `
 const footerContent = () => `
-        <div>All Saints Los Angeles</div>
-        <div><a href="https://www.instagram.com/allsaintschurchla/" target="_blank"><img src="img/instagram logo_icon.png" alt=""></a></div>
-        <div><a href="https://chat.whatsapp.com/KKgRO6k60Us7ofspO42nbq" target="_blank"><img src="img/whatsapp logo_icon.png" alt=""></a></div>
-        <div><a href="https://open.spotify.com/show/73BmDDi6yqUY6CptdBgDLD?si=38b2ba9631ef49b4" target="_blank"><img src="img/icons8-spotify-50.png" alt=""></a></div>
-        <div><a href="mailto:admin@allsaintsla.church">admin@allsaintsla.church</a></div>
-`        
-        export {headerContent, footerContent}
+                <div>All Saints Los Angeles</div>
+                <div><a href="https://www.instagram.com/allsaintschurchla/" target="_blank"><img src="img/instagram logo_icon.png" alt=""></a></div>
+                <div><a href="https://chat.whatsapp.com/KKgRO6k60Us7ofspO42nbq" target="_blank"><img src="img/whatsapp logo_icon.png" alt=""></a></div>
+                <div><a href="https://open.spotify.com/show/73BmDDi6yqUY6CptdBgDLD?si=38b2ba9631ef49b4" target="_blank"><img src="img/icons8-spotify-50.png" alt=""></a></div>
+                <div><a href="mailto:admin@allsaintsla.church">admin@allsaintsla.church</a></div>
+        `        
+function underlineCurrentPage() {
+    const links = document.querySelectorAll("#navbar a");
+    const currentPage = window.location.pathname.split("/").pop(); // e.g. "about.html"
+    
+    links.forEach(link => {
+        if (link.getAttribute("href") === currentPage) {
+            link.style.textDecoration = "underline";  // direct inline style
+            // OR add a class:
+            // link.classList.add("active");
+        }
+    });
+}        
+
+export {headerContent, signupContent, footerContent, underlineCurrentPage}
--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-import {headerContent, footerContent} from "./head-foot.js"
+import {headerContent, signupContent, footerContent, underlineCurrentPage} from "./head-foot.js"
 
 header = document.getElementById("header")
+signup = document.getElementById("signup")
 footer = document.getElementById("footer")
 
 header.innerHTML = headerContent()
+signup.innerHTML = signupContent()
 footer.innerHTML = footerContent()
 
+document.addEventListener("DOMContentLoaded", underlineCurrentPage);
